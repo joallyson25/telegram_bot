9b2e6348118b4fb796a6ac1c5cf7142cd023057e
Joallyson Castro <joallyson25@gmail.com>
2019-03-05 23:35:11 -0300

    Add files via upload

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,6 +1,35 @@
-from chatbot import *
+from chatbot import Chatbot
+import pyttsx3  # reprodução em voz do texto
+import speech_recognition as sr  # Reconhecimento de voz
 
-Bot = Chatbot("Carlos")
+# Inicializa a engine e define o idioma da voz
+en = pyttsx3.init()
+en.setProperty('voice', 'brazil')
+rec = sr.Recognizer()
+
+
+# Herença da classe Chatbot e polimorfismo
+class BotFalante(Chatbot):
+
+    def escuta(self, frase=None):
+        try:
+            with sr.Microphone() as mic:
+                fala = rec.listen(mic)
+            frase = rec.recognize_google(fala, language='pt')
+            print(frase)  # Qual texto ele está identificando
+        except sr.UnknownValueError:
+            print('Erro ao identificar a voz')
+            return ''
+        # Chamando a função antiga do Chatbot
+        return super().escuta(frase=frase)
+
+    def fala(self, frase):
+        en.say(frase)
+        en.runAndWait()
+        super().fala(frase)
+
+
+Bot = BotFalante('Felipe')
 
 while True:
 
